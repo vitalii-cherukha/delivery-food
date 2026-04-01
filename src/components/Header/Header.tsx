3850f7a83e629c571ae6ede69482debdd89626e8
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-04-01 13:35:31 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router";
+import Container from "../Container/Container";
 import css from "./Header.module.css";
 
 const Header = () => {
-  return <div className={css.header}>Header</div>;
+  return (
+    <header className={css.header}>
+      <Container>
+        <nav className={css.nav}>
+          <ul className={css.list}>
+            <li className={css.item}>
+              <Link className={css.link} to="/shop">
+                Shop
+              </Link>
+            </li>
+            <li className={css.item}>
+              <Link className={css.link} to="/shopping-cart">
+                Shopping Cart
+              </Link>
+            </li>
+            <li className={css.item}>
+              <Link className={css.link} to="/history">
+                History
+              </Link>
+            </li>
+            <li className={css.item}>
+              <Link className={css.link} to="/coupons">
+                Coupons
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;

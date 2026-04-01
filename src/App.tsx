44d474e8473f380ffd8e61d23e994cb0e948c250
vitalii_cherukha <vitalii.cherukha@gmail.com>
2026-04-01 13:26:01 +0300

    init routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import MainLayout from "./components/MainLayout/MainLayout";
+import Shop from "./pages/Shop/Shop";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
+import History from "./pages/History/History";
+import Coupons from "./pages/Coupons/Coupons";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="shop" element={<Shop />} />
+          <Route path="shopping-cart" element={<ShoppingCart />} />
+          <Route path="history" element={<History />} />
+          <Route path="coupons" element={<Coupons />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
